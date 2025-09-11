cbef704774b10864571bf371d168a0e331d2c9e1
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2025-09-11 14:15:58 +0300

    settings

--- djangoset/storage_backends.py
+++ djangoset/storage_backends.py
@@ -2,9 +2,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from boto3.s3.transfer import TransferConfig
 
 DEFAULT_TRANSFER_CONFIG = TransferConfig(
-    multipart_threshold=15 * 1024 * 1024,  # файлы < 15MB — обычный PUT
-    max_concurrency=2,                     # меньше потоков
-    multipart_chunksize=5 * 1024 * 1024,   # размер части для больших файлов
+    multipart_threshold=15 * 1024 * 1024,
+    max_concurrency=2,
+    multipart_chunksize=5 * 1024 * 1024,
     use_threads=True
 )
 
@@ -17,11 +17,4 @@ class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
-    transfer_config = DEFAULT_TRANSFER_CONFIG
-
-class PrivateMediaStorage(S3Boto3Storage):
-    location = 'private'
-    default_acl = 'private'
-    file_overwrite = False
-    custom_domain = False
     transfer_config = DEFAULT_TRANSFER_CONFIG
