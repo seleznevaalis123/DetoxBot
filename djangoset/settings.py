4f7a0e73dd373ea26fced0d54d5bffc4c9ac5605
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2025-09-11 14:44:47 +0300

    settings

--- djangoset/settings.py
+++ djangoset/settings.py
@@ -76,6 +76,7 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # <- самое первое
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -84,8 +85,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
+
 
 
 CORS_ALLOWED_ORIGINS = [
