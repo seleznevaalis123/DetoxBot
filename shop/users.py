b567f1e23d806960bf5c11d0054a35f99b5a6476
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2025-09-12 16:54:18 +0300

    edit DB_PASSWORD

--- shop/users.py
+++ shop/users.py
@@ -13,7 +13,7 @@ def connect_to_db():
         host=os.getenv('DB_HOST'),
         port=os.getenv('DB_PORT'),
         user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASS')
+        password=os.getenv('DB_PASSWORD')
     )
 
 
