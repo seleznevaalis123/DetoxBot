dc7b85df2f2ce20d4cb3f4eed33d4114c9a56299
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2025-10-24 19:49:25 +0200

    add mutations add_card create_order

--- shop/schema.py
+++ shop/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from djangoset.types import CategoryType, TeaItemsType, CardsType, OrdersType
-from shop.models import Category, TeaItems, Cards, Orders, Users
+from shop.models import Category, TeaItems, Cards, Orders, Users, OrderItems
 
 
 class Query(graphene.ObjectType):
@@ -24,3 +24,67 @@ class Query(graphene.ObjectType):
     def resolve_user_order(root, info, tg_id):
         user = Users.objects.get(tg_id=tg_id)
         return Orders.objects.filter(user=user)
+
+
+# Mutation
+class AddToCart(graphene.Mutation):
+    class Arguments:
+        tg_id = graphene.String(required=True)
+        tea_item_id = graphene.Int(required=True)
+        quantity = graphene.Int(required=False, default_value=1)
+
+    ok = graphene.Boolean()
+    card = graphene.Field(CardsType)
+
+    @staticmethod
+    def mutate(root, info, tg_id, tea_item_id, quantity):
+        user = Users.objects.get(tg_id=tg_id)
+        tea_item = TeaItems.objects.get(id=tea_item_id)
+        card, created = Cards.objects.get_or_create(
+            user=user,
+            tea_item=tea_item,
+            defaults={
+                "quantity": quantity,
+                "item_price": tea_item.item_price_idr,
+                "currency": "RUB",
+            },
+        )
+        if not created:
+            card.quantity += quantity
+            card.save()
+        return AddToCart(ok=True, card=card)
+
+
+class CreateOrder(graphene.Mutation):
+    class Arguments:
+        tg_id = graphene.String(required=True)
+        delivery_address = graphene.String(required=True)
+
+    ok = graphene.Boolean()
+    order = graphene.Field(OrdersType)
+
+    @staticmethod
+    def mutate(root, info, tg_id, delivery_address):
+        user = Users.objects.get(tg_id=tg_id)
+        cart_items = Cards.objects.filter(user=user)
+        if not cart_items.exists():
+            raise Exception("Cart is empty")
+        order = Orders.objects.create(user=user, status="NEW", delivery_address=delivery_address)
+        for card in cart_items:
+            OrderItems.objects.create(
+                order=order,
+                tea_item=card.tea_item,
+                price=card.item_price,
+                quantity=card.quantity,
+                currency=card.currency
+            )
+        cart_items.delete()
+        return CreateOrder(ok=True, order=order)
+
+
+class Mutation(graphene.ObjectType):
+    add_to_cart = AddToCart.Field()
+    create_order = CreateOrder.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
