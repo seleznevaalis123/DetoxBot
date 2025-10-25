e7cc11017f04c8adb2e53fbb36d2385e6105febb
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2025-10-25 10:25:53 +0200

    cart update schema

--- djangoset/schema.py
+++ djangoset/schema.py
@@ -1,13 +1,13 @@
 import graphene
 from shop.schema import Query as ShopQuery
-from shop.schema import AddToCart, CreateOrder
+from shop.schema import UpdateCart, CreateOrder
 
 
 class Query(ShopQuery, graphene.ObjectType):
     pass
 
 class Mutation(graphene.ObjectType):
-    add_to_cart = AddToCart.Field()
+    updateCart = UpdateCart.Field()
     create_order = CreateOrder.Field()
 
 
