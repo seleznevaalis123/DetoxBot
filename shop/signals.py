665de1e38587f34b0a3b5986ada5628e51f0db7b
Alisa Selezneva <89939174+seleznevaalis123@users.noreply.github.com>
2026-01-24 00:43:16 +0800

    email

--- shop/signals.py
+++ shop/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Orders
+import threading
 
 
 @receiver(post_save, sender=Orders)
@@ -11,17 +12,26 @@ def notify_manager_on_order(sender, instance, created, **kwargs):
     if not created:
         return
 
-    def send_email():
-        print("EMAIL: sending started")
+    def send_email_safe():
+        print("EMAIL SENDING STARTED")
 
-        send_mail(
-            subject=f"Новый заказ #{instance.id}",
-            message=f"Создан новый заказ.\n\n{instance}",
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=["selezneva.test@ya.ru"],
-            fail_silently=False,
-        )
+        try:
+            send_mail(
+                subject=f"Новый заказ #{instance.id}",
+                message=f"Создан новый заказ.\n\nЗаказ ID: {instance.id}",
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=["selezneva.test@ya.ru"],
+                fail_silently=False,
+            )
+            print("EMAIL SENT OK")
 
-        print("EMAIL: sending finished")
+        except Exception as e:
+            print("EMAIL ERROR:", repr(e))
 
-    transaction.on_commit(send_email)
+
+    transaction.on_commit(
+        lambda: threading.Thread(
+            target=send_email_safe,
+            daemon=True
+        ).start()
+    )
